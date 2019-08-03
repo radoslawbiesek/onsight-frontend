920aeebe090302e38b50464de8f3816c7a1c853d
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-08-03 22:18:19 +0200

    Add sort-by select options

--- src/components/Products.js
+++ src/components/Products.js
@@ -13,9 +13,13 @@ class Products extends React.Component {
             items: this.props.items,
             itemsLength: this.props.items.length,
 
+            // sorting
+            sortBy: 'A-Z',
+
             // pagination 
             showing: 6,
             page: 1,
+            pages: Math.ceil(this.props.items.length / 6),
             prevPage: null,
             nextPage: null,
             showingStart: 1,
@@ -25,11 +29,13 @@ class Products extends React.Component {
     }
 
     updatePage(page) {
+        if (page > this.state.pages) return;
         let showingStart = 1 + (page - 1) * this.state.showing;
         let showingStop = Math.min(page * this.state.showing, this.state.itemsLength);
         this.setState({
             showingStart,
-            showingStop
+            showingStop,
+            page
         })
     }
 
@@ -39,6 +45,10 @@ class Products extends React.Component {
 
     handleClick = (id) => {
         this.props.addToCart(id);
+    }
+
+    handleChange = (event) => {
+        this.setState({sortBy: event.target.value});
     }
 
     render() {
@@ -57,9 +67,10 @@ class Products extends React.Component {
         });
 
         let pages = [];
-        for (let i = 0; i < (Math.ceil(this.state.itemsLength / this.state.showing)); i++) {
+        for (let i = 0; i < this.state.pages; i++) {
             pages.push(
                 <li 
+                    key={`page${i}`}
                     onClick={()=>this.updatePage(i+1)}
                     className='products__page'
                 >
@@ -70,7 +81,20 @@ class Products extends React.Component {
 
         return (
             <div>
-                <p className='products__showing'>Showing {this.state.showingStart} - {this.state.showingStop} of {this.state.itemsLength} results</p>
+                <div className='products__top-nav'>
+                    <form className='products__sort-by'>
+                        <label>
+                            Sort by:
+                            <select onChange={this.handleChange}>
+                                <option selected value="A-Z">A-Z</option>
+                                <option value="Z-A">Z-A</option>
+                                <option value="Lowest Price">Lowest Price</option>
+                                <option value="Highest Price">Highest Price</option>
+                            </select>
+                        </label>
+                    </form>
+                    <p className='products__showing'>Showing {this.state.showingStart} - {this.state.showingStop} of {this.state.itemsLength} results</p>
+                </div>
                 <div className='products-grid'>
                     {itemsList.slice(this.state.showingStart-1,this.state.showingStop)}
                 </div>
