70bc58966eac2fb060d6e43cc0e6644cde9ae904
Radosław <radoslaw.biesek@gmail.com>
2020-04-05 14:41:01 +0200

    Format code in Footer and Hero

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -2,28 +2,35 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Footer.css';
 
-function Footer() {
-    return (
-        <footer className='footer'>
-            <p className='footer__text'>
-                <a className='footer__link' rel="noopener noreferrer" href='https://github.com/radoslawbiesek' target='_blank'>© 2019 OnSight. All rights reserved.</a>
-            </p>
-            <ul className='footer__menu'>
-                <li className='footer__item'>
-                    <NavLink className='footer__link' exact to={`/`}>Shop</NavLink>
-                </li>
-                <li className='footer__item'>
-                    <NavLink className='footer__link'>FAQ</NavLink>
-                </li>
-                <li className='footer__item'>
-                    <NavLink className='footer__link'>Contact</NavLink>
-                </li>
-                <li className='footer__item'>
-                    <NavLink className='footer__link'>Blog</NavLink>
-                </li>
-            </ul>
-        </footer>
-    )
-}
+const Footer = () => (
+  <footer className='footer'>
+    <p className='footer__text'>
+      <a
+        className='footer__link'
+        rel='noopener noreferrer'
+        href='https://github.com/radoslawbiesek'
+        target='_blank'
+      >
+        © 2019 OnSight. All rights reserved.
+      </a>
+    </p>
+    <ul className='footer__menu'>
+      <li className='footer__item'>
+        <NavLink className='footer__link' to={`/`}>
+          Shop
+        </NavLink>
+      </li>
+      <li className='footer__item'>
+        <NavLink className='footer__link'>FAQ</NavLink>
+      </li>
+      <li className='footer__item'>
+        <NavLink className='footer__link'>Contact</NavLink>
+      </li>
+      <li className='footer__item'>
+        <NavLink className='footer__link'>Blog</NavLink>
+      </li>
+    </ul>
+  </footer>
+);
 
 export default Footer;
--- src/components/Layout/Hero/Hero.js
+++ src/components/Layout/Hero/Hero.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './Hero.css';
 
-function Hero() {
-    return (
-        <div className="hero">
-            <h1 className="hero__title">Climbing Store</h1>
-            <h2 className="hero__subtitle">Shop Now</h2>
-        </div>
-    );
-}
+const Hero = () => (
+  <div className='hero'>
+    <h1 className='hero__title'>Climbing Store</h1>
+    <h2 className='hero__subtitle'>Shop Now</h2>
+  </div>
+);
 
 export default Hero;
