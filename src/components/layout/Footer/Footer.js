2c06fe51dd428e8f43420690bdca039420b05975
Radosław <radoslaw.biesek@gmail.com>
2020-04-23 20:34:12 +0200

    Install emotion. Refactor footer to styled component. Add global styles

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,36 +1,76 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Footer.css';
+import styled from '@emotion/styled';
 
-const Footer = () => (
-  <footer className='footer'>
-    <p className='footer__text'>
-      <a
-        className='footer__link'
-        rel='noopener noreferrer'
-        href='https://github.com/radoslawbiesek'
-        target='_blank'
-      >
-        © 2019 OnSight. All rights reserved.
-      </a>
-    </p>
-    <ul className='footer__menu'>
-      <li className='footer__item'>
-        <NavLink className='footer__link' to={`/`}>
-          Shop
-        </NavLink>
-      </li>
-      <li className='footer__item'>
-        <NavLink className='footer__link'>FAQ</NavLink>
-      </li>
-      <li className='footer__item'>
-        <NavLink className='footer__link'>Contact</NavLink>
-      </li>
-      <li className='footer__item'>
-        <NavLink className='footer__link'>Blog</NavLink>
-      </li>
-    </ul>
-  </footer>
-);
+const StyledFooter = styled('footer')`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  min-height: 115px;
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: 0.9px;
+
+  & ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+    & li {
+      margin: 0 25px;
+    }
+  }
+
+  & p {
+    opacity: 0.5;
+  }
+
+  & a:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+
+  @media screen and (max-width: 992px) {
+    flex-direction: column;
+
+    & ul {
+      order: -1;
+      margin-top: 25px;
+    }
+    & p {
+      text-align: center;
+    }
+  }
+`;
+
+const Footer = () => {
+  const navLinks = [
+    { label: 'Shop' },
+    { label: 'FAQ' },
+    { label: 'Contact' },
+    { label: 'Blog' },
+  ];
+
+  return (
+    <StyledFooter>
+      <p>
+        <a
+          rel='noopener noreferrer'
+          href='https://github.com/radoslawbiesek'
+          target='_blank'
+        >
+          © 2020 OnSight. All rights reserved.
+        </a>
+      </p>
+      <ul>
+        {navLinks.map((link) => (
+          <li>
+            <NavLink to={link.to ? link.to : '/'}>{link.label}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </StyledFooter>
+  );
+};
 
 export default Footer;
